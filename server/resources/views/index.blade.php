1747131856cf945941c5955792793a971a860ed6
Rajkaran Singh Grewal <rajkaran.singh.grewal5@gmail.com>
2021-01-01 12:05:05 +0530

    installed vue project to get Vue3 working

--- server/resources/views/index.blade.php
+++ server/resources/views/index.blade.php
@@ -8,5 +8,10 @@
 <body>
     <div id="app"></div>
 </body>
-<script src="/js/app"></script>
+<script src="/app/dist/js/app.c73052d4.js"></script>
+<script src="/app/dist/js/chunk-2d0a484a.bed15ded.js"></script>
+<script src="/app/dist/js/chunk-vendors.5aefa87d.js"></script>
+<script src="/app/dist/js/chunk-2d0af7b0.1d8e29a5.js"></script>
+<script src="/app/dist/js/chunk-2d2253a7.66d07123.js"></script>
+
 </html>
